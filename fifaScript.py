028fd3a28aca24ccd647f5298d0073ae3290113c
Pedro Sastre <pedro2000sastre@gmail.com>
2024-02-21 14:57:36 +0100

    fix variable names

--- fifaScript.py
+++ fifaScript.py
@@ -10,6 +10,8 @@ df = pd.read_csv(r"C:\Users\Pedro\Documents\PythonScripts\fifa21_raw_data.csv")
     #Show all the columns#
 pd.set_option('display.max_columns', None)
 
+    #Convert the height and weight columns to numerical forms
+
     #Change 'Height' column to an int data type
 
 #df['Height'] = df['Height'].astype(int)
@@ -17,9 +19,9 @@ pd.set_option('display.max_columns', None)
     # but it will throw error because some data contains non-numeric characters (such as single quote and double quote),
     # so it cannot be converted directly to an integer.
 
-tipodedatos = df.dtypes
+dataTypesAnalysis = df.dtypes
 
-print(tipodedatos)
+print(dataTypesAnalysis)
 
 print(df["Height"].dtype)
 
@@ -37,6 +39,8 @@ df['Weight']=df['Weight'].str.replace('lbs','')
 
 df['Weight']=df['Weight'].astype(int)
 
+#####Remove the unnecessary newline characters from all columns that have them.
+
     #Clean the 'Team & Contract' column, a lot of '\n'
 
 df['Team & Contract']=df['Team & Contract'].str.replace("\n", "")
@@ -46,6 +50,8 @@ df['Team & Contract']=df['Team & Contract'].str.replace("\n", "")
 print(df['Joined'].dtype)
 
 df['Joined'] = pd.to_datetime(df['Joined'], format='%b %d, %Y')
+
+print(df['Joined'].dtype)
 
 actual_date = datetime.now()
 
@@ -58,23 +64,24 @@ df_filter_newplayers = df[df['Joined']<limit_date]
 
 columns_to_int = ['Value', 'Wage', 'Release Clause']
 for column in columns_to_int:
-    df[column] = df[column].replace('[\€,M,K]','',regex=True).astype(float)*1e6
+    df[column] = df[column].replace('[\€,M]','',regex=True).astype(float)*1e6
+    df[column] = df[column].replace('[\€,K]','',regex=True).astype(float)*1e3
     df[column] = df[column].astype(int)
 
     #Some columns have 'star' characters. Strip those columns of these stars and make the columns numerical
     
-columns_to_int = ['W/F', 'SM', 'IR']
-for column in columns_to_int:
+columns_to_int_star = ['W/F', 'SM', 'IR']
+for column in columns_to_int_star:
     df[column] = df[column].str.replace('★','')
     df[column] = df[column].astype(int)
 
     #Which players are highly valuable but still underpaid (on low wages)? (hint: scatter plot between wage and value)
 
-df['scatter_plot'] = df['Value'] / df['Wage']
+df['simple_correlation'] = df['Value'] / df['Wage']
 
     #Its sort the information with the height values at the top
 
-df_order_sp = df.sort_values(by='scatter_plot', ascending=False) 
+df_order_sc = df.sort_values(by='scatter_plot', ascending=False) 
 
     #Filter all the rows that have Value and Wage equal to cero
 
